b5bee958e34e78cf7a2a29f4e1df68ada09f6610
Sofiyan-Mohammed <b24it039@kitsw.ac.in>
2024-11-22 22:15:47 +0530

    Add files via upload

---
+++ 002.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main(){
+
+int dec=10;
+
+float f=3.14;
+
+char ch='p';
+
+//printf("Enter an integer, float value, character value: ");
+
+//scanf("%d%f%c", &dec, &f, &ch);
+
+printf("%d %f %c\n", dec, f,ch);
+
+}
